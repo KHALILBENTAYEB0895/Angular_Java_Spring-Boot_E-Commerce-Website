9b123a406fd538a7e46cf1e6a9d62286a8314dd7
KalilBENTAYEB <khalil.bentayeb95@gmail.com>
2024-10-16 00:19:50 +0100

    Add to cart from Details product view

--- 03-frontend/ecom-angular/src/app/components/product-details/product-details.component.ts
+++ 03-frontend/ecom-angular/src/app/components/product-details/product-details.component.ts
@@ -1,7 +1,9 @@
+import { CartService } from './../../services/cart.service';
 import { ProductService } from './../../services/product.service';
 import { Product } from './../../common/product';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartItem } from '../../common/cart-item';
 
 @Component({
   selector: 'app-product-details',
@@ -13,7 +15,8 @@ export class ProductDetailsComponent implements OnInit{
   product!: Product;
 
   constructor(private productService: ProductService,
-              private route: ActivatedRoute){}
+              private route: ActivatedRoute,
+              private cartService: CartService){}
   
   ngOnInit(){
     this.route.paramMap.subscribe(() => {
@@ -29,6 +32,12 @@ export class ProductDetailsComponent implements OnInit{
         this.product = data;
       }
     )
+  }
+
+  addToCart() {
+    console.log(`Adding to cart : ${this.product.name}, ${this.product.unitPrice}`);
+    const theCartItem = new CartItem(this.product);
+    this.cartService.addToCart(theCartItem);
   }
   
 }
