f43f99b864a46601f2f33be2f119b4a6739fc17a
KalilBENTAYEB <khalil.bentayeb95@gmail.com>
2024-10-23 23:36:17 +0100

    fixing the billing adress bug

--- 03-frontend/ecom-angular/src/app/components/checkout/checkout.component.ts
+++ 03-frontend/ecom-angular/src/app/components/checkout/checkout.component.ts
@@ -102,6 +102,7 @@ export class CheckoutComponent implements OnInit{
       this.checkoutFormGroup.controls['billingAddress'].setValue(
         this.checkoutFormGroup.controls['shippingAddress'].value
       );
+      this.billingAddressStates = this.shippingAddressStates;
     } else {
       this.checkoutFormGroup.controls['billingAddress'].reset();
     }
