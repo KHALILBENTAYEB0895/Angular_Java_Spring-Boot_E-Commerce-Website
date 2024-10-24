c2bff5ade7e2820222c0fe29d675e220c5ea9162
KalilBENTAYEB <khalil.bentayeb95@gmail.com>
2024-10-24 18:08:32 +0100

    Add checkout Form validation

--- 03-frontend/ecom-angular/src/app/components/checkout/checkout.component.ts
+++ 03-frontend/ecom-angular/src/app/components/checkout/checkout.component.ts
@@ -1,6 +1,6 @@
 import { ShopFromService } from './../../services/shop-from.service';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Country } from '../../common/country';
 import { State } from '../../common/state';
 
@@ -31,9 +31,11 @@ export class CheckoutComponent implements OnInit{
   ngOnInit(): void {
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
-        firstName: [''],
-        lastName: [''],
-        email: ['']
+        firstName: new FormControl('',[Validators.required, Validators.minLength(2)]),
+        lastName: new FormControl('',[Validators.required, Validators.minLength(2)]),
+        email: new FormControl('',
+          [Validators.required, Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$')]
+        )
       }),
       shippingAddress: this.formBuilder.group({
         street: [''],
@@ -92,8 +94,9 @@ export class CheckoutComponent implements OnInit{
   
   onSubmit(){
     console.log("Handling the submit button");
-    console.log(this.checkoutFormGroup.get('customer')?.value);
-    console.log(this.checkoutFormGroup.get('billingAddress')?.value);
+    if(this.checkoutFormGroup.invalid){
+      this.checkoutFormGroup.markAllAsTouched();
+    }
   }
 
   copyAddress(event: Event) {
@@ -135,5 +138,9 @@ export class CheckoutComponent implements OnInit{
 
 
   }
+
+  get firstName() {return this.checkoutFormGroup.get('customer.firstName'); }
+  get lastName() {return this.checkoutFormGroup.get('customer.lastName'); }
+  get email() {return this.checkoutFormGroup.get('customer.email'); }
 
 }
