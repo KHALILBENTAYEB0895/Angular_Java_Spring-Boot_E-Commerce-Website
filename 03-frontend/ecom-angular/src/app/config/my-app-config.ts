6616f54110c8a801b2655e882842015f72f0fb6c
KalilBENTAYEB <khalil.bentayeb95@gmail.com>
2024-10-27 19:57:03 +0100

    install Okta dependencies

---
+++ 03-frontend/ecom-angular/src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+    oidc: {
+        clientId: '0oakokr98gYpQDVTh5d7',
+        issuer: 'https://dev-69678114.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+}
